d00754b107ceacdc858d1647407ecfa9f15f5b19
Hemantdhote <hemantdhote76@gmail.com>
2024-02-24 04:16:13 +0530

    first day

---
+++ script.js
@@ -0,0 +1,97 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var image=document.querySelector(".loader img");
+
+
+
+
+
+
+// var tl=gsap.timeline();
+
+
+// tl.from(".imageholder img",{
+//     y:480,
+//     duration:1,
+//     delay:1.5,
+//     stagger:1
+// })
+// tl.from(".textholder h2",{
+//     y:180,
+//     duration:1,
+//     stagger:1
+// })
+
+// gsap.to(".loader",{
+//     y:"-130%",
+//     duration:2,
+//     delay:3.3
+
+// })
+// gsap.from(".fixed",{
+//     y:"-100%",
+//     duration:1,
+//     delay:4
+// })
